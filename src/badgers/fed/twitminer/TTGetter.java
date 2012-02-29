1f8ce6f0fb7743815e1f0c1b9f5e9f9e836ace06
Eric Gillet <fear_hardcore@yahoo.fr>
2012-02-29 01:26:20 +0100

    Getting top trends works for every location, need to fix Worldwide location because it have no name

--- src/badgers/fed/twitminer/TTGetter.java
+++ src/badgers/fed/twitminer/TTGetter.java
@@ -1,22 +1,14 @@
 package badgers.fed.twitminer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-
 import twitter4j.Location;
 import twitter4j.ResponseList;
-import twitter4j.Status;
 import twitter4j.Trend;
 import twitter4j.Trends;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
 
 public class TTGetter {
 	
@@ -24,20 +16,39 @@ public class TTGetter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        try {
-        	Twitter twitter = new TwitterFactory().getInstance();
-        	
+        Twitter twitter = new TwitterFactory().getInstance();
+        AccessToken acc = new AccessToken("15770377-lxF00DWbbjaqr2TE0STRLA69NrInWWE19Iku8CEAj", "DTZd1igxzXKstsHIUZi9KD15LW1KQXrfp6lvVhpE");
+        twitter.setOAuthAccessToken(acc);
+        try{
+        	Trends tr;
+        	Date date;
+        	String li = null;
         	ResponseList<Location> locations = twitter.getAvailableTrends();
-        	Trends wwtrends =  twitter.getLocationTrends(1);
-        	Trend [] ttt = wwtrends.getTrends();
-        	Date date = wwtrends.getAsOf();
-        	String li = date.toString() + "; WorldWide";
-        	for (Trend i : ttt)
+        			
+        	for (Location i : locations)
         	{
-        		li += "; " + i.getName();
+        		try{
+        		tr = twitter.getLocationTrends(i.getWoeid());
+        		date = tr.getAsOf();
+        		Trend [] ttt = tr.getTrends();
+        		li = date + "; ";
+        		String cname = i.getCountryName();
+        		if (cname == null)
+        			//Fixme, il faut gérer le cas worldwide 
+        			li += "Worldwide";
+        		else
+        			li+= cname;
+        		
+        		for (Trend t : ttt)
+        		{
+        			li += "; " + t.getName();
+        		}
+        		System.out.println(li);
+        		} catch(TwitterException te)
+        		{
+        			te.printStackTrace();
+        		}
         	}
-        	System.out.println(li);
             System.exit(0);
         } catch (TwitterException te) {
             te.printStackTrace();
